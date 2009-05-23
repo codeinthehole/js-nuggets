5f41ba6e2b0abedf36adcf992486e1ea8d189cdd
David Winterbottom <david.winterbottom@gmail.com>
2009-05-23 19:24:51 +0100

    First import of data

---
+++ README.markdown
@@ -0,0 +1,11 @@
+Javascript nuggets
+==================
+
+Just a simple collection javascript utilities.
+
+Unit tests
+----------
+Each test has a corresponding unit test suite (written using the [http://www.jsunit.net/](jsunit) framework).
+To run the tests, ensure that the folders are checked out to a web-readable folder, 
+then point your browser to the test runner page at utils/jsunit/testRunner.html.  From there, you can paste in 
+the path to the appropriate test file and run the tests.
---
+++ scripts/inspect.js
@@ -0,0 +1,35 @@
+/**
+ * Simple Javascript object inspection methods
+ *
+ * Author:  David Winterbottom 
+ * Website: http://codeinthehole.com
+ * License: Creative Commons Attribution-Share Alike 3.0
+ *          http://creativecommons.org/licenses/by-sa/3.0/
+ * Version: 0.1
+ */
+
+var Inspect = {
+    TYPE_FUNCTION: 'function',
+    // Returns an array of (the names of) all methods
+	methods: function(obj) {
+        var testObj = obj || self;
+        var methods = [];
+	    for (prop in testObj) {
+            if (typeof testObj[prop] == Inspect.TYPE_FUNCTION && typeof Inspect[prop] != Inspect.TYPE_FUNCTION) {
+                methods.push(prop);
+            }
+        }
+	    return methods;
+	},
+	// Returns an array of (the names of) all properties
+	properties: function(obj) {
+		var testObj = obj || self;
+        var properties = [];
+	    for (prop in testObj) {
+            if (typeof testObj[prop] != Inspect.TYPE_FUNCTION && typeof Inspect[prop] != Inspect.TYPE_FUNCTION) {
+                properties.push(prop);
+            }
+        }
+	    return properties;
+	}
+}
